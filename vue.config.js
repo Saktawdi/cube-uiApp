10c3aa594df0531dfb509692af3caabda8cfd3de
Sakta_wdi <1742804029@qq.com>
2022-08-14 22:59:45 +0800

    parallel: false【解决构建项目Cannot read property ‘__vueMarkdownOptions__‘ of undefined】

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-
+  parallel:false,
   css: {
     loaderOptions: {
       stylus: {
